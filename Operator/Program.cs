c735dbd50b06fe62cbf79a14a938b57f6b7ce312
alefFSilva <alefs93@gmail.com>
2016-11-23 21:39:30 -0200

    Novos exercícios

---
+++ Operator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Sintaxe
+            int a = 10;
+            int b = 2;
+
+            int soma = a + b;
+            int subt = a - b;
+            int mult = a * b;
+            int rest = a % b; //Módulo
+
+            //Incremento  - x++
+            //Decremento  - x--
+
+            //Comparativos
+
+            if (a > b)
+                // Console.WriteLine("a > b");
+
+                if (b < a)
+                    // Console.WriteLine("b < a");
+
+                    // => - Lambda
+                    if (a >= b)
+                        //    Console.WriteLine("a >= b");
+
+                        if (b <= a)
+                            //   Console.WriteLine("b <= a");
+
+                            //Equals
+                            if (a == b)
+                                //  Console.WriteLine("a == b");
+
+                                //Not
+                                if (a != b)
+                                    // Console.WriteLine("a != b");
+
+                                    //Lógicos
+                                    //XOR  - Lógico Exclusivo  - Um ou outro
+                                    if (true ^ false)
+                                        //Console.WriteLine("true");
+
+                                        //Condicionais
+
+                                        //AND
+                                        if (true && true)
+                                            //    Console.WriteLine("true");
+
+                                            //OR 
+                                            //   if (true || false)
+                                            // Console.WriteLine("true");
+
+                                            #endregion
+
+
+            Console.WriteLine("Valor 1: ");
+            int v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2: ");
+            int v2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Soma: ");
+            Console.WriteLine("Subtração: ");
+            Console.WriteLine("Multiplicação: ");
+            Console.WriteLine("Divisão: ");
+        }
+    }
+}
---
+++ anonymousTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nullable
+            int? i = null;
+            string a = null;
+
+            var valores = new[] {
+                                new { idade = 18, nome = "Alef" },
+                                new { idade = 20, nome = "João"}
+            };
+
+            foreach (var item in valores)
+            {
+                Console.WriteLine("Nome: " + item.nome);
+                Console.WriteLine("Idade: " + item.idade.ToString());
+                Console.WriteLine("--------------------------------------");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
