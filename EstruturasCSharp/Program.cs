3d1f56e62f90a2533824c279914df5562956dfe9
Alef <Alef@DESKTOP-LITV1MO>
2016-12-06 21:51:50 -0200

    Novas aulas - C# Avançado

---
+++ EstruturasCSharp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasCSharp
+{
+    public class PosicaoClass{
+
+        public int x;
+        public int y;
+
+        public override string ToString()
+        {
+            return "X = " + x.ToString() + ", " + "Y = " + y.ToString();
+        }
+
+    }
+
+    public struct PosicaoStruct
+    {
+        public int x;
+        public int y;
+        public override string ToString()
+        {
+            return "X = " + x.ToString() + ", " + "Y = " + y.ToString();
+        }
+
+      
+    }
+
+    public enum Roles
+    {
+        Developer, Arquiteto, Tester
+    }
+
+    public enum Colors
+    {
+        Red = 0, Green = 1, Blue = 2
+    }
+
+    public enum FormasPagto
+    {
+        Boleto, Cartao, Cheque
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PosicaoStruct pos1 = new PosicaoStruct();
+            pos1.x = 10;
+            pos1.y = 20;
+
+            PosicaoStruct pos2 = new PosicaoStruct();
+            pos2.x = 30;
+            pos2.y = 40;
+
+            PosicaoStruct pos3 = pos2;
+            pos3.x = 50;
+
+            Console.WriteLine(pos1);
+            Console.WriteLine(pos2);
+            Console.WriteLine(pos3);
+
+            FormasPagto formaPagto = FormasPagto.Boleto;
+            Roles role = Roles.Arquiteto;
+            Colors cor;
+            cor = 0;
+
+            Console.WriteLine(formaPagto);
+            Console.WriteLine(role);
+            Console.WriteLine(cor);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ valueRef/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valueRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y = 20;
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
