bc5f675e7baea2441cada7203dd42fe44b825f1a
Alef <Alef@DESKTOP-LITV1MO>
2016-12-07 20:52:44 -0200

    Modifiers e GetSet

---
+++ GetSets/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSets
+{
+
+    public class Conta
+    {
+
+        private string _Cliente;
+        private double _Valor;
+
+
+        public Conta(string nome)
+        {
+            this._Cliente = nome;
+        }
+
+        public string getCliente()
+        {
+            return this._Cliente;
+        }
+
+        public void setCliente(string value)
+        {
+            this._Cliente = value;
+        }
+
+        public void Sacar(Double valor)
+        {
+
+            this._Valor = this._Valor - valor;
+
+        }
+
+        public void Deposita(int valor)
+        {
+
+            this._Valor = this._Valor + valor;
+
+        }
+
+
+        public double getSaldo()
+        {
+            return this._Valor;
+        }
+    
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conta c = new Conta("Alef");
+            c.Deposita(500);
+            c.Sacar(250);
+
+            Console.WriteLine("Cliente: " + c.getCliente());
+            Console.WriteLine("Saldo: " + c.getSaldo());
+            Console.ReadLine();
+
+
+        }
+    }
+}
