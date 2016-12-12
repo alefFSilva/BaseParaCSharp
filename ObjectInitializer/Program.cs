c8a9d5fa1c278a72f49e3d259ec374d1178e74eb
Alef <Alef@DESKTOP-LITV1MO>
2016-12-12 20:40:54 -0200

    Object Initializaer

---
+++ ObjectInitializer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializer
+{
+    public class Filme
+    {
+        public string Nome;
+        public int ano;
+
+    }
+
+    public class Autor
+    {
+        public int Codigo;
+        public string Nome;
+        public List<Filme> Filmes ;
+
+        //public Autor(int codigo, string nome, string filme)
+        //{
+        //    this.Codigo = codigo;
+        //    this.Nome = nome;
+        //    this.Filme = filme;
+
+        //}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Autor a = new Autor()
+            {
+                Codigo = 2,
+                Nome = "Wolverine",
+                Filmes = new List<Filme>()
+                {
+                    new Filme()
+                    {
+                        Nome = "X-Man",
+                        ano = 2010
+                    },
+
+
+            new Filme()
+            {
+                Nome = "Wolverin Origins",
+                ano = 2013
+            }
+                }
+            
+            };
+
+        }
+    }
+}
