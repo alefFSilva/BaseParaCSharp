e9e1811bd30ab2fbc6cd796350a11b7b3246f40d
Alef <Alef@DESKTOP-LITV1MO>
2016-12-12 21:27:49 -0200

    Inference Type e Dynamic

---
+++ TypeInference_Dynamic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInference_Dynamic
+{
+    public class Connection
+    {
+        public string Banco;
+
+        public void Conectar()
+        {
+            Console.WriteLine("Conectando");
+        }
+
+        public void CloseConnction()
+        {
+            Console.WriteLine("Fechando conexão.");
+        } 
+    }
+
+    public static class Fabrica
+    {
+        public static Connection CriarConnection()
+        {
+            return new Connection();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inferência - Resolvido em tempo de compilação
+            var con = Fabrica.CriarConnection();
+            con.Conectar();
+
+            //Dynamic - Resolvido em tempo de execução, quando ainda não se conhece o objeto to handle
+            dynamic con1  = Fabrica.CriarConnection();
+
+
+        }
+    }
+}
