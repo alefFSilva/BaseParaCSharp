1eb45c316e3fccd42c4f238beb4391053ea56a54
Alef <Alef@DESKTOP-LITV1MO>
2017-02-05 23:11:25 -0200

    Projeto3Camadas BLL DAL DTO

---
+++ ClassesGenericas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesGenericas
+{
+    class Pilha<T>
+    {
+        string nome;
+        int codigo;
+
+        int pos = 0;
+
+        T[] itens = new T[110];
+
+        public void Push(T item)
+        {
+            itens[pos] = item;
+            pos++;
+        }
+
+        public T Pop()
+        {
+            pos--;
+            return itens[pos];
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var c = new Pilha<string>();
+            c.Push("Alef");
+
+         
+
+
+
+
+        }
+    }
+}
